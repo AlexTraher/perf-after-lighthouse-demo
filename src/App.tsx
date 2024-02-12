fadee3f7ba925acfe3e1a6f3387a3449ff6aa9bb
Alex Traher <alextraher@Gemmas-MacBook-Air.local>
2024-02-12 17:21:23 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import GameControls from './game-controls/GameControls'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <GameControls />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/game-controls/GameControls.tsx
@@ -0,0 +1,13 @@
+import { Card, CardHeader } from "@/components/ui/card";
+import { FC } from "react";
+
+const GameControls: FC<{}> = () => {
+  return (
+    <Card className="w-full">
+      <CardHeader>Controls</CardHeader>
+    </Card>
+    
+  )
+}
+
+export default GameControls;
